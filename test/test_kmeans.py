a1cedfb18109ed688e0655d2f36c128ab9c9e9eb
almonteloya <ana.almonteloya@gmail.com>
2022-02-09 22:54:44 -0800

    Identation test

--- test/test_kmeans.py
+++ test/test_kmeans.py
@@ -10,28 +10,28 @@ from cluster import (
 from scipy.spatial.distance import cdist
 
 
-def kmean_test():
+def test_kmeans():
 
 	""" 
 	Check K means algorithm 
 	Test: raise error when k=0
 	"""
 	try:
-    	kmeans = KMeans(k=0)
-        assert False
-    except ValueError:
-        assert True
+		kmeans = KMeans(k=0)
+		assert False
+	except ValueError:
+		assert True
 
 	"""
 	Test: when k=1 all the label predicted should be only one group
 	"""
 
 	clusters, labels = make_clusters(n=50, k=1,scale=1)
-    km = KMeans(k=1)
-    km.fit(clusters)
-    pred = km.predict(clusters)
-    ## all the pred are the same 
-    assert(len(set(pred))== 1)
+	km = KMeans(k=1)
+	km.fit(clusters)
+	pred = km.predict(clusters)
+	## all the pred are the same 
+	assert(len(set(pred))== 1)
 
 
 	"""
@@ -41,21 +41,20 @@ def kmean_test():
 	Because we set the seed we know the exact number of points per cluster. 
 	Check that the proportion of the predicted label is the same as the generated label.
 	"""
-
-    clusters, labels = make_clusters(n=100, k=2,scale=.1)
-    km = KMeans(k=2)
-    km.fit(clusters)
-    pred = km.predict(clusters)
+	clusters, labels = make_clusters(n=100, k=2,scale=.1)
+	km = KMeans(k=2)
+	km.fit(clusters)
+	pred = km.predict(clusters)
 
     ## convert to list so we can count
-    l_pred=pred.tolist()
-    labels_l=labels.tolist()
+	l_pred=pred.tolist()
+	labels_l=labels.tolist()
     
-    a={l_pred.count(0),l_pred.count(1)}
-    b={labels_l.count(0),labels_l.count(1)}
+	a={l_pred.count(0),l_pred.count(1)}
+	b={labels_l.count(0),labels_l.count(1)}
     
     ## There should be no differences between the two sets 
-    assert(len(a.difference(b)) == 0)
+	assert(len(a.difference(b)) == 0)
 
 
 	"""
@@ -64,13 +63,12 @@ def kmean_test():
 
 	"""
 
-    clusters, labels = make_clusters(k=70)
-    km = KMeans(k=70)
-    km.fit(clusters)
-    pred = km.predict(clusters)
+	clusters, labels = make_clusters(k=70)
+	km = KMeans(k=70)
+	km.fit(clusters)
+	pred = km.predict(clusters)
     
-    assert (len(set(pred)) == 70)
+	assert (len(set(pred)) == 70)
 
-    return()
+test_kmeans()
 
-kmean_test()
